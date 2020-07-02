38ac0aa65615949f21bcc18f4db803bb6d571d7c
Silfrido Benitez <silfrido123chiro@gmail.com>
2020-07-02 23:10:35 +0100

    switch toggle

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-const check = document.getElementById('check');
+// const btnCheck = document.getElementById('.checkbox');
 
-const card = document.querySelector('.main-cost');
+// const card = document.querySelector('.main-cost');
 
-check.addEventListener('change', e => {
+// check.addEventListener('change', e => {
 
-    card.classList.toggle('toggle');
+//     card.classList.toggle('toggle');
+
+
+// });
+
+const btnChek = document.querySelector('.checkbox');
+btnChek.addEventListener('click', function() {
+    document.getElementById('bmensual').classList.toggle('active');
+    document.getElementById('banual').classList.toggle('active');
+
+    document.getElementById('pmensual').classList.toggle('active');
+    document.getElementById('panual').classList.toggle('active');
+
+    document.getElementById('mmensual').classList.toggle('active');
+    document.getElementById('manual').classList.toggle('active');
 
 
 });
